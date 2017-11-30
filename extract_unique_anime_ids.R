103d1e6550c4135b473c9dc38714fe6b5a108d0f
Noah Johnson <noah.johnson13@ncf.edu>
2017-11-30 15:33:20 -0500

    got anime_ids.txt for input to Rogers scraping

--- extract_unique_anime_ids.R
+++ extract_unique_anime_ids.R
@@ -1,5 +1,9 @@
-anime_table_path <- 'data/anime.csv'
-
-animes <- read.csv(anime_table_path, header = TRUE)
+animes <- read.csv('data/anime.csv', header = TRUE)
 
 print(head(animes))
+
+unique_ids <- unique(animes$anime_id)
+
+fOut <- file("data/output.txt")
+writeLines(unique_ids, fOut)
+close(fOut)
