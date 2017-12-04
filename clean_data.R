26776d2807af83c63cea28891de1402ebbae0b46
Noah Johnson <noah.johnson13@ncf.edu>
2017-12-04 01:17:18 -0500

    Normalized genres table, cleaned ratings csv to rid NAs, which put it under 100 MB, so added it to repo.

--- clean_data.R
+++ clean_data.R
@@ -1,26 +1,32 @@
 library(dplyr)
+library(tidyr)
 
 # Animes
 animes <- read.csv('data/raw/anime.csv', header = TRUE)
-
+animes$genre <- as.character(animes$genre)
 print(head(animes))
 
 # Create file of unique anime ids, for web scraping
 unique_ids <- unique(animes$anime_id)
 
 fOut <- file("data/clean/anime_ids.txt")
-write(unique_ids, fOut, ncolumns = 1) # one id per line
+write.csv(unique_ids, fOut, ncolumns = 1) # one id per line
 close(fOut)
 
 # Normalize the data by splitting genres into a separate table
-genres <- data.frame()
+genre_df <- data.frame(id = animes$anime_id, genre = animes$genre, stringsAsFactors=FALSE)
+genre_df <- genre_df %>% unnest(genre = strsplit(genre, ","))
+genre_df$genre <- trimws(genre_df$genre)
+fOut <- file('data/clean/genres.csv')
+write.csv(genre_df, fOut, row.names = FALSE)
+close(fOut)
 
 # Ratings
 ratings <- read.csv('/home/noah/Downloads/anime-recommendations-database/rating.csv', header = TRUE)
 
 # Filter the raw data so that we only keep rows where the user gave the anime a rating
 # (MAL ratings are between 1 and 10)
-clean_ratings <- ratings %>% 
-fOut <- file('/home/noah/Downloads/anime-recommendations-database/clean_ratings.csv')
-write(ratings_clean, fOut, ncolumns = length(ratings))
+clean_ratings <- ratings %>% filter(1 <= rating & rating <= 10)
+fOut <- file('data/raw/no_null_ratings.csv')
+write.csv(clean_ratings, fOut, row.names = FALSE)
 close(fOut)
