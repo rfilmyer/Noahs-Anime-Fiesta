8dcd995e21ba486d1b4b325fa36d0872d915ea5f
Noah Johnson <noah.johnson13@ncf.edu>
2017-12-05 00:26:22 -0500

    Created culture score in presentation

--- src/clean_data.R
+++ src/clean_data.R
@@ -2,14 +2,13 @@ library(dplyr)
 library(tidyr)
 
 # Animes
-animes <- read.csv('data/raw/anime.csv', header = TRUE)
-animes$genre <- as.character(animes$genre)
+animes <- read.csv('../data/raw/anime.csv', header = TRUE, stringsAsFactors = FALSE)
 print(head(animes))
 
 # Create file of unique anime ids, for web scraping
 unique_ids <- unique(animes$anime_id)
 
-fOut <- file("data/clean/anime_ids.txt")
+fOut <- file("../data/clean/anime_ids.txt")
 write.csv(unique_ids, fOut, ncolumns = 1) # one id per line
 close(fOut)
 
@@ -17,7 +16,7 @@ close(fOut)
 genre_df <- data.frame(id = animes$anime_id, genre = animes$genre, stringsAsFactors=FALSE)
 genre_df <- genre_df %>% unnest(genre = strsplit(genre, ","))
 genre_df$genre <- trimws(genre_df$genre)
-fOut <- file('data/clean/genres.csv')
+fOut <- file('../data/clean/genres.csv')
 write.csv(genre_df, fOut, row.names = FALSE)
 close(fOut)
 
@@ -27,6 +26,6 @@ ratings <- read.csv('/home/noah/Downloads/anime-recommendations-database/rating.
 # Filter the raw data so that we only keep rows where the user gave the anime a rating
 # (MAL ratings are between 1 and 10)
 clean_ratings <- ratings %>% filter(1 <= rating & rating <= 10)
-fOut <- file('data/raw/no_null_ratings.csv')
+fOut <- file('../data/raw/no_null_ratings.csv')
 write.csv(clean_ratings, fOut, row.names = FALSE)
 close(fOut)
